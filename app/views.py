00ac19144cd8ddcfd6e5d536e078c669897aa779
www <www@USA.(none)>
2017-08-12 13:23:02 -0400

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+from app import app
+from flask import request
+@app.route('/')
+@app.route('/index')
+def index():
+    return "hello world\n"
+
+@app.route('/updateLog',methods=['GET','POST'])
+def updateLog():
+    data = request.get_json()
+    print(data)
+    return "{'errormsg':'success'}"
