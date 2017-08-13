ece85869d0eea37d8e4b06049271b97132a44896
jzjwonderful <jzjwonderful@gmail.com>
2017-08-14 07:54:59 +0800

    add try block

--- app/views.py
+++ app/views.py
@@ -2,17 +2,29 @@
 # coding=utf-8
 from app import app
 from flask import request
+
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     return "hello world\n"
 
+
+
 @app.route('/updateLog',methods=['GET','POST'])
 def updateLog():
-    data = request.get_json()
-    print(data)
-    return "{'errormsg':'success'}"
+    try:
+        data = request.get_json()
+        return data
+    finally:
+        return "{'errormsg':'success'}"
+    
+   
 
 @app.route('/log.html',methods=['GET'])
 def logWeb():
     return "null log data"
+
+def process_data(threadName,delay):
+    pass
