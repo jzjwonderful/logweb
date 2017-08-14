f426f499b4fb83719e9f933b85c501d2d9fc3b77
jzjwonderful <jzjwonderful@gmail.com>
2017-08-14 08:07:23 +0800

    add index response

--- app/views.py
+++ app/views.py
@@ -2,13 +2,13 @@
 # coding=utf-8
 from app import app
 from flask import request
-
+from flask import render_template
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index.html')
 def index():
-    return "hello world\n"
+    return render_template("index.html")
 
 
 
